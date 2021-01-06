9692699db338cd0b5b6f0ff03ebf6d29f251a985
Issa Fabiala <issakarissefabiala@gmail.com>
2021-01-06 17:07:27 +0100

    Javascript

---
+++ JavaScript/1.Intro/sandbox.js
@@ -0,0 +1,84 @@
+//voorbeeld
+        //string concatenation
+        let firstName = 'Brandon';
+        let lastName = 'Sanderson';
+        
+        let fullName = firstName + ' ' + lastName;
+        console.log(fullName);
+        
+        //getting characters
+        console.log(fullName[0]);
+
+//opdracht 1
+let getal1;
+console.log("getal1" + getal1);
+
+//opdracht 2.1
+// let getal2;
+// console.log("getal2" + getal2);
+
+//opdracht 2.2
+let getal2 = 2;
+console.log("getal2" + getal2);
+
+// //opdracht 3
+// let getal3 = 3;
+// console.log("getal3" + getal3);
+
+// //opdracht 4
+let getal3 = 5;
+console.log("getal3" + getal3);
+
+//opdracht 5
+let datatype = 'true';  
+console.log("datatype" + typeof datatype);
+
+//opdracht 6.b
+let getal = 2;
+console.log("getal2" + getal2);
+
+//opdracht 6.c
+// alert(1n + 2n); // 3
+
+// alert(5n / 2n); // 2
+
+//opdracht 6.d
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+
+// alert(id1 == id2); // false
+
+//opddracht 7
+let getal6 = 6;
+let getal8 = 8;
+
+console.log(getal6 + getal8);
+console.log(getal8 - getal6);
+console.log(getal6 * getal8);
+console.log(getal8 / getal6);
+
+//opdracht 8
+let som = 27;
+console.log(som);
+
+
+//opdracht 9
+// let number = 3;
+// number--;
+
+// console.log(number--);
+
+// let number = 3;
+// const sum = number++ + 2;
+// console.log(sum);
+
+// let number = 3;
+// const sum = ++number + 2;
+// console.log(sum);
+
+//opdracht 10
+let number = 3;
+number += 4;
+console.log(number);
+
+
