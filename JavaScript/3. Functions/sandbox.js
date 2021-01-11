c69e2941fd9ee7b08bee358705fb2b69a31090be
Issa Fabiala <issakarissefabiala@gmail.com>
2021-01-11 13:45:56 +0100

    Javascript onderdeel conditions

---
+++ JavaScript/3. Functions/sandbox.js
@@ -0,0 +1,11 @@
+let tekstHello = "Hello World";
+let number1 = 2;
+let number2 = 4;
+console.log();
+
+function answer () {
+    // label.innerHTML = tekstHello;
+    label.innerHTML = number1 * number2;
+    
+}
+
