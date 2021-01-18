3996928bea775580ede75db0a59e4b24cc835660
Issa Fabiala <issakarissefabiala@gmail.com>
2021-01-18 16:53:12 +0100

    Javascript Scope

---
+++ JavaScript/5. Arrays/sandbox.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+let numbers = [];
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log (numbers[2]);
+
+
+//opdrachten 2
+let fruiten = [];
+
+fruiten = ['peer', 'appel', 'banaan', 'kersen'];
+
+// console.log (fruiten[1]);
+
+//opdracht 3
+
+const fruit = (element) => element > 5;
+
+console.log (fruiten[1]);
+console.log(fruiten.findIndex(fruit[1]));
