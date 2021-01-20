36fd2c301789db6ebd234ab340c6522bf25bcec4
Issa Fabiala <issakarissefabiala@gmail.com>
2021-01-20 11:45:30 +0100

    Javascript Array 8 & 9

--- JavaScript/5. Arrays/sandbox.js
+++ JavaScript/5. Arrays/sandbox.js
@@ -8,7 +8,7 @@ console.log (numbers[2]);
 //opdrachten 2
 let fruiten = [];
 
-fruiten = ['peer', 'appel', 'banaan', 'kersen', 'aardbei'];
+fruiten = ['peer', 'appel', 'mandarijn', 'kersen', 'aardbei'];
 
 console.log (fruiten[1]);
 
@@ -22,16 +22,24 @@ const fruit = fruiten.indexOf("aardbei");
 
 //opdracht 5
 
-const randomNumber = numbers[Math.round(Math.random() *4)];
-console.log(randomNumber);
+// const randomNumber = numbers[Math.round(Math.random() *4)];
+// console.log(randomNumber);
 
-let randomFruit = fruiten;
-console.log (randomFruit[randomNumber]);
+// let randomFruit = fruiten;
+// console.log (randomFruit[randomNumber]);
 
-let a = randomNumber % fruiten.length;
-console.log(a);
+// let a = randomNumber % fruiten.length;
+// console.log(a);
 
 // fruits= numbers % fruiten;
+
+let math = Math.floor(Math.random()* numbers.length);
+console.log(math);
+
+console.log(fruiten[math % 5]);
+
+
+
 
 
 // document.getElementById("randomFruiten").innerHTML = fruits;
@@ -58,13 +66,24 @@ console.log(a);
 // var fruits = ["Banana", "Orange", "Apple", "Mango"];
 // document.getElementById("demo").innerHTML = fruits;
 
-function myFunction() {
-fruits.sort();
-document.getElementById("demo").innerHTML = fruits;
-}
+// function myFunction() {
+// fruits.sort();
+// document.getElementById("demo").innerHTML = fruits;
+// }
 
 //Opdracht 7
 console.log(fruiten.length);
+
+//opdracht 8
+fruiten.splice(1, 1, "sinaasappel");
+document.getElementById('Opdracht8').innerHTML = fruiten;
+console.log(fruiten);
+
+//opdracht 9
+
+[fruiten[2], fruiten[4]] = [fruiten[4], fruiten[2]];
+document.getElementById('Opdracht9').innerHTML = fruiten;
+console.log(fruiten);
 
 
 
