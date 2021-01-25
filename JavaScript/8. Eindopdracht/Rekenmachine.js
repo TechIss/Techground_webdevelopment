d2256018b5ec100558777afeb262bd101bfb52eb
Issa Fabiala <issakarissefabiala@gmail.com>
2021-01-25 16:49:20 +0100

    Javascript Eindopdracht Begin Javascript
    Rekenmachine variabelen

---
+++ JavaScript/8. Eindopdracht/Rekenmachine.js
@@ -0,0 +1,30 @@
+//Variabelen
+// knoppen, de knoppen moeten werken, en de ingevulde value moet getoond worden in de ledscherm.
+// De value in het scherm moet met elkaar kunnen "rekenen"
+// Als je op de "=" teken drukt moet de uitkomst in totaal in de scherm moeten komen.
+
+const Numbs = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+];
+console.log('Numbs:', Numbs)
+
+
+const ItsDoingThings = [
+    "AC",
+    "PlusMinus",
+    "Modulo",
+    "Divide",
+    "Multiply",
+    "Minus",
+    "Answer",
+    "Plus",
+    "Komma",
+];
+
+console.log('ItsDoingThings:', ItsDoingThings)
+
+
+function numbers() {
+    document.getElementById(/).onclick = function(){numberFunction()};
+    document.getElementById("displayAnswer"),innerHTML = "/";
+}
