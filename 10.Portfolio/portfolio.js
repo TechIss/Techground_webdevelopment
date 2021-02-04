8a4ae45ee706eaeb2038fa337565861a8735dad8
Issa Fabiala <issakarissefabiala@gmail.com>
2021-02-04 09:07:10 +0100

    Portfolio voor fase deel 1.3

--- 10.Portfolio/portfolio.js
+++ 10.Portfolio/portfolio.js
@@ -1,34 +1,42 @@
-const containerOctagon = document.getElementById("containerOctagon");
-const Octagons = 1503;
-const color = [
-    '#083c40',
-    '#3d575b',
-    '#6b898b'
-];
-
-for (let i = 0; i < Octagons; i++) {
-    const items = document.createElement('div');
-    items.classList.add('Octagon');
-    items.addEventListener('mouseover', () => setColor(items));
-    items.addEventListener('mouseout', () => removeColor(items));
-    containerOctagon.appendChild(items);
+var cursor = document.getElementById('cursor');
+window.onmousemove = function(e){
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
 }
 
-function setColor (element){
-    const colors = getRandomColor();
-    element.style.boxShadow = `0 0 2px ${colors}`;
-    element.style.background = colors;
-}
+// const containerHexagon = document.getElementById("containerHexagon");
+// const hexagons = 1503;
+// const color = [
+//     '#083c40',
+//     '',
+//     '#3d575b',
+//     '#6b898b'
+// ];
 
-function removeColor (element){
-    element.style.background = "";
-    console.log(removeColor);
-}
+// for (let i = 0; i < hexagons; i++) {
+//     const items = document.createElement('div');
+//     items.classList.add('hexagon');
+//     items.addEventListener('mouseover', () => setColor(items));
+//     items.addEventListener('mouseout', () => removeColor(items));
+//     containerHexagon.appendChild(items);
+// }
+
+// function setColor (element){
+//     const colors = getRandomColor();
+//     element.style.boxShadow = `0 0 2px ${colors}`;
+//     element.style.background = colors;
+// }
+
+// function removeColor (element){
+//     element.style.background = "";
+//     console.log(removeColor);
+// }
 
 
-function getRandomColor () {
-    return color [Math.floor(Math.random()*color.length)]
-}
+// function getRandomColor () {
+//     return color [Math.floor(Math.random()*color.length)]
+// }
+
 
 
 
